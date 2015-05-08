975f891f293fc530f7c30f1afbcb9a8943d30da8
Jerry Preissler <gerald.preissler@innoq.com>
2015-05-08 11:46:22 +0200

    reworked handling of project directory

--- df_activator/resources/start-activator.sh
+++ df_activator/resources/start-activator.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-export PROJECTROOT=/var/projects
-
-case "$1" in
-	--new)
-		cd $PROJECTROOT
-		su develop -c "/opt/activator/activator new"
-		;;
-	*)
-		if [ -f $PROJECTROOT/$1/activator ];
-		then
-			cd $PROJECTROOT/$1
-			su develop -c "./activator shell"
-		else
-			echo "No activator project found in " $1
-		fi
-		;;
-esac
+export PROJECTROOT=/var/project
+cd $PROJECTROOT
+export CMD="/opt/activator/activator "$@
+echo su develop -c $CMD
