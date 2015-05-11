233f7e669bffdce4eb9038258641421235655a69
Jerry Preissler <gerald.preissler@innoq.com>
2015-05-11 20:27:12 +0200

    updated activator url and switched to mini version to save space in image

--- df_activator/build_image.sh
+++ df_activator/build_image.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
 # grab the json describing the latest version of activator
-export val=$(curl https://typesafe.com/activator/latest)
+export val=$(curl https://www.typesafe.com/activator/latest)
 # extract url to mini version
-export url=$(echo $val | jq '.url' | tr -d '"')
+export url=$(echo $val | jq '.miniUrl' | tr -d '"')
 export FILE="./downloads/"$(basename $url)
 
 if [ -f $FILE ];
